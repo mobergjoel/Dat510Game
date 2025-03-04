7fc3280f10cc1504e65d0334b6d02c08fba4b11c
Tell-Becker <lwtb04@gmail.com>
2025-03-04 14:19:03 +0100

    Finished all key Spawnpoints

--- Dat510Game/Assets/Script/KeySpawn.cs
+++ Dat510Game/Assets/Script/KeySpawn.cs
@@ -9,32 +9,43 @@ public class KeySpawn : MonoBehaviour
     private Vector3 randomSpawnPoint2;
     private Vector3 randomSpawnPoint3;
 
+    private Dictionary<Transform, string> spawnPointParents = new Dictionary<Transform, string>();
 
     public GameObject key1;
     public GameObject key2;
     public GameObject key3;
     void Start()
     {
+        foreach (var spawnPoint in spawnPoints)
+        {
+            spawnPointParents[spawnPoint] = spawnPoint.parent.name;
+        }
         SpawnKey();
     }
 
     // Update is called once per frame
     void SpawnKey()
     {
-        randomSpawnPoint1 = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
-        randomSpawnPoint2 = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
-        while(randomSpawnPoint2 == randomSpawnPoint1)
+        List<Transform> selectedSpawnPoints = new List<Transform>();
+        HashSet<string> usedParentLocations = new HashSet<string>();
+
+        while (selectedSpawnPoints.Count < 3)
         {
-            randomSpawnPoint2 = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
+            Transform potentialSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+            string parentLocation = spawnPointParents[potentialSpawnPoint];
+
+            if(!usedParentLocations.Contains(parentLocation) && !selectedSpawnPoints.Contains(potentialSpawnPoint))
+            {
+                selectedSpawnPoints.Add(potentialSpawnPoint);
+                usedParentLocations.Add(parentLocation);
+            }
         }
-        randomSpawnPoint3 = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
-        while(randomSpawnPoint3 == randomSpawnPoint1 || randomSpawnPoint3 == randomSpawnPoint2)
-        {
-            randomSpawnPoint3 = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
-        }
-        Instantiate(key1, randomSpawnPoint1, Quaternion.identity);
-        Instantiate(key2, randomSpawnPoint2, Quaternion.identity);
-        Instantiate(key3, randomSpawnPoint3, Quaternion.identity);
+
+
+
+        Instantiate(key1, selectedSpawnPoints[0].position, selectedSpawnPoints[0].rotation);
+        Instantiate(key2, selectedSpawnPoints[1].position, selectedSpawnPoints[1].rotation);
+        Instantiate(key3, selectedSpawnPoints[2].position, selectedSpawnPoints[2].rotation);
 
     }
 }
